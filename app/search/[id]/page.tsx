d76e4199634e2953feba1775260897beb9ddf892
Brandon Mitchell <brandonmitchell217@gmail.com>
2023-11-19 22:12:23 -0500

    [fix]: added padded to search page

--- app/search/[id]/page.tsx
+++ app/search/[id]/page.tsx
@@ -2,16 +2,26 @@
 import React from "react";
 import { useSearchQuery } from "../../GlobalRedux/api/searchSlice";
 import SearchPage from "../_components/SearchPage";
+import { dummyArr } from "@/lib/util";
+import CardSkeleton from "@/components/Media/CardSkeleton";
 
 export default function Index({ params: { id } }: { params: { id: string } }) {
-  const { data, error, isLoading } = useSearchQuery(id); // this does work btw
+  const { data, error, isLoading } = useSearchQuery(id);
 
   console.log(data);
 
   return (
     <div>
-      <div className="container">
-        {isLoading ? <span> Loading...</span> : <SearchPage query={data} />}
+      <div className="container px-4 xl:px-0">
+        {isLoading ? (
+          dummyArr.map((i) => (
+            <div key={i}>
+              <CardSkeleton />
+            </div>
+          ))
+        ) : (
+          <SearchPage query={data} />
+        )}
       </div>
     </div>
   );
--- app/search/_components/SearchPage.tsx
+++ app/search/_components/SearchPage.tsx
@@ -7,24 +7,26 @@ export default function SearchPage({ query }: any) {
   const results = query.results;
   // console.log(query);
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-10">
-      {results.map((result: any) => (
-        <Card key={result.id} data={result} />
-        // <div key={result.id}>
-        //   <div className="relative h-72 md:h-56 lg:h-72 xl:h-80">
-        //     <Image
-        //       src={`${IMG_PATH}${
-        //         result.poster_path ? result.poster_path : result.backdrop_path
-        //       }`}
-        //       alt={`result poster`}
-        //       fill
-        //       sizes="100%"
-        //       className="rounded-md object-cover"
-        //     />
-        //   </div>
-        //   {/* <h3 className="text-lg">{result.original_title ? result.original_title : result.title}</h3> */}
-        // </div>
-      ))}
+    <div className="py-24 flex flex-col items-center">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-10">
+        {results.map((result: any) => (
+          <Card key={result.id} data={result} />
+          // <div key={result.id}>
+          //   <div className="relative h-72 md:h-56 lg:h-72 xl:h-80">
+          //     <Image
+          //       src={`${IMG_PATH}${
+          //         result.poster_path ? result.poster_path : result.backdrop_path
+          //       }`}
+          //       alt={`result poster`}
+          //       fill
+          //       sizes="100%"
+          //       className="rounded-md object-cover"
+          //     />
+          //   </div>
+          //   {/* <h3 className="text-lg">{result.original_title ? result.original_title : result.title}</h3> */}
+          // </div>
+        ))}
+      </div>
     </div>
   );
 }
