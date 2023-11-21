14cdf025f7704080904d7b359fa5728997d85275
Brandon Mitchell <brandonmitchell217@gmail.com>
2023-11-20 23:46:08 -0500

    [feat]: added next auth, sign in/out

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import TestFetch from "@/components/TestFetch";
 import PopularCarousel from "@/components/Carousel/Home/PopularCarousel";
 import Landing from "@/components/Carousel/Home/Landing";
 import UpcomingCarousel from "@/components/Carousel/Home/UpcomingCarousel";
+import { getServerSession } from "next-auth";
 
 const trendingFilters = [
   { type: "movie", label: "day" },
@@ -14,12 +15,30 @@ const popularFilters = [
   { type: "tv", label: "tv" },
 ];
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession();
   return (
     <main className="pb-20">
       <div className="text-white space-y-20">
         {/* <TestFetch /> */}
         <Landing />
+
+        {session?.user?.name ? (
+          <div className="container ">
+            <div className="space-y-2 text-center">
+              <h1 className="text-4xl font-bold">
+                Welcome {session?.user?.name}
+              </h1>
+              <p>Find new movies or shows!</p>
+            </div>
+          </div>
+        ) : (
+          <div className="container">
+            <h1 className="text-4xl font-bold text-center">Welcome</h1>
+            <p>You should sign in!</p>
+          </div>
+        )}
+
         <UpcomingCarousel title="Upcoming Movies" id="upcoming_movies" />
         <TrendingCarousel
           title="Trending Movies"
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,7 +1,50 @@
+"use client";
 import Link from "next/link";
 import React from "react";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function Nav() {
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <nav className="fixed z-50 top-0 left-0 right-0 py-8 bg-black/80">
+        <div className="container flex items-center justify-between shadow-md px-4 md:px-0">
+          <Link href="/" className="text-white font-bold">
+            TMDB
+            <span className="text-red-400 ml-1">Next</span>
+          </Link>
+
+          <div className="flex items-center space-x-8">
+            <ul className="flex gap-6 items-center font-medium">
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/movies">Movies</Link>
+              </li>
+              <li>
+                <Link href="/tvShows">TV Shows</Link>
+              </li>
+              <li>
+                <Link href="/people">People</Link>
+              </li>
+            </ul>
+
+            <div>
+              <button
+                className="px-4 py-1 bg-red-400 text-black rounded-md"
+                type="button"
+                onClick={() => signOut()}
+              >
+                Sign Out
+              </button>
+            </div>
+          </div>
+        </div>
+      </nav>
+    );
+  }
   return (
     <nav className="fixed z-50 top-0 left-0 right-0 py-8 bg-black/80">
       <div className="container flex items-center justify-between shadow-md px-4 md:px-0">
@@ -9,20 +52,33 @@ export default function Nav() {
           TMDB
           <span className="text-red-400 ml-1">Next</span>
         </Link>
-        <ul className="flex gap-6 items-center font-medium">
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/movies">Movies</Link>
-          </li>
-          <li>
-            <Link href="/tvShows">TV Shows</Link>
-          </li>
-          <li>
-            <Link href="/people">People</Link>
-          </li>
-        </ul>
+
+        <div className="flex items-center space-x-8">
+          <ul className="flex gap-6 items-center font-medium">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/movies">Movies</Link>
+            </li>
+            <li>
+              <Link href="/tvShows">TV Shows</Link>
+            </li>
+            <li>
+              <Link href="/people">People</Link>
+            </li>
+          </ul>
+
+          <div>
+            <button
+              className="px-4 py-1 bg-red-400 text-black rounded-md"
+              type="button"
+              onClick={() => signIn()}
+            >
+              Sign In
+            </button>
+          </div>
+        </div>
       </div>
     </nav>
   );
