16c672df896291278fd85aab7bbba6897574fdca
Brandon Mitchell <brandonmitchell217@gmail.com>
2023-11-30 22:14:57 -0500

    removing export again

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -18,6 +18,6 @@ const authOptions: NextAuthOptions = {
   ],
 };
 
-export const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
